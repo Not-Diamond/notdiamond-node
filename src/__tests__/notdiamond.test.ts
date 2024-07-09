7d1f301febfac3efc06160f62638ccedf45ae07d
Zachary Lee <w.zachary.lee@gmail.com>
2024-07-09 11:57:19 -0400

    fixed

--- src/__tests__/notdiamond.test.ts
+++ src/__tests__/notdiamond.test.ts
@@ -7,7 +7,6 @@ import {
   NotDiamondErrorResponse,
   NotDiamondOptions,
   Provider,
-  Tool,
 } from '../notdiamond';
 
 const messages: Message[] = [{ content: 'What is 12x12?', role: 'user' }];
@@ -19,38 +18,6 @@ const llmProviders: Provider[] = [
   {
     provider: 'anthropic',
     model: 'claude-3-opus-20240229',
-  },
-];
-const tools: Tool[] = [
-  {
-    type: 'function',
-    function: {
-      name: 'add',
-      description: 'Adds a and b.',
-      parameters: {
-        type: 'object',
-        properties: {
-          a: { type: 'integer' },
-          b: { type: 'integer' },
-        },
-        required: ['a', 'b'],
-      },
-    },
-  },
-  {
-    type: 'function',
-    function: {
-      name: 'multiply',
-      description: 'Multiplies a and b.',
-      parameters: {
-        type: 'object',
-        properties: {
-          a: { type: 'integer' },
-          b: { type: 'integer' },
-        },
-        required: ['a', 'b'],
-      },
-    },
   },
 ];
 
@@ -83,49 +50,6 @@ describe('NotDiamond', () => {
         tradeoff: 'latency',
         timeout: 3,
         default: 1,
-      };
-
-      jest.spyOn(global, 'fetch').mockImplementation(() =>
-        Promise.resolve({
-          ok: false,
-          json: () => Promise.resolve({ detail: 'Error occurred' }),
-        } as Response),
-      );
-
-      const response = (await notDiamond.modelSelect(
-        options,
-      )) as NotDiamondErrorResponse;
-      expect(response.detail).toBe('Error occurred');
-    });
-  });
-
-  describe('modelSelect should use tools', () => {
-    it('should return success response', async () => {
-      const options: ModelSelectOptions = {
-        messages: [
-          {
-            role: 'system',
-            content: 'You are a mathematician with tools at your disposal.',
-          },
-          { role: 'user', content: 'What is 234 + 82234?' },
-        ],
-        llmProviders,
-        tools,
-      };
-
-      const response = (await notDiamond.modelSelect(
-        options,
-      )) as ModelSelectSuccessResponse;
-      expect(response.providers).toBeDefined();
-      expect(response.session_id).toBeDefined();
-    });
-
-    it('should return error response on failure', async () => {
-      const options: ModelSelectOptions = {
-        messages,
-        llmProviders,
-        tools,
-        tradeoff: 'latency',
       };
 
       jest.spyOn(global, 'fetch').mockImplementation(() =>
